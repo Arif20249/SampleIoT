ab358b9ca091cc61813fa286b05f13824e4caad0
Arif20249 <137975520+Arif20249@users.noreply.github.com>
2023-09-15 23:05:31 -0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,6 +19,14 @@ const routes: Routes = [
     path: 'dashboard',
     loadChildren: () => import('./pages/dashboard/dashboard.module').then( m => m.DashboardPageModule)
   },
+  {
+    path: 'panel-control',
+    loadChildren: () => import('./pages/panel-control/panel-control.module').then( m => m.PanelControlPageModule)
+  },
+  {
+    path: 'menu',
+    loadChildren: () => import('./pages/menu/menu.module').then( m => m.MenuPageModule)
+  },
 ];
 
 @NgModule({
